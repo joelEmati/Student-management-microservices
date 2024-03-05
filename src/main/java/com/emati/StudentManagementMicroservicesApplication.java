11522a077188325ad851f8e7272e7c0d14f83b5c
JoelEmati <jelemati@gmail.com>
2024-03-05 21:43:18 +0100

    init

---
+++ src/main/java/com/emati/StudentManagementMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.emati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementMicroservicesApplication.class, args);
+	}
+
+}
